98f2997b89fc0f4d629276dab27a71367b37476b
kalemadaniel <kalemadaniel2014@gmail.com>
2022-03-15 09:57:23 +0200

    first commit

---
+++ src/demo_java_mysql_picture/Demo_Java_Mysql_Picture.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package demo_java_mysql_picture;
+
+/**
+ *
+ * @author Ulb-Kalema
+ */
+public class Demo_Java_Mysql_Picture {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+    }
+    
+}
